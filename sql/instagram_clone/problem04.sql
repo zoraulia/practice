8bb92234c66168fc6138de22e13c0907fd07bb56
zoraulia <56012719+zoraulia@users.noreply.github.com>
2021-07-17 20:24:39 +0700

    add problem04 ig clone sql file

---
+++ sql/instagram_clone/problem04.sql
@@ -0,0 +1,15 @@
+# IDENTIFY MOST POPULAR PHOTO (AND USER WHO CREATED IT)
+
+SELECT 
+    username,
+    photos.id,
+    photos.image_url, 
+    COUNT(*) AS total
+FROM photos
+INNER JOIN likes
+    ON likes.photo_id = photos.id
+INNER JOIN users
+    ON photos.user_id = users.id
+GROUP BY photos.id
+ORDER BY total DESC
+LIMIT 1;
