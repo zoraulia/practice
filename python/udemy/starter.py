b1622f30977002b8c26f3c46094d33a6432bc557
zoraulia <56012719+zoraulia@users.noreply.github.com>
2021-08-11 20:16:25 +0700

    modify starter file

--- python/udemy/starter.py
+++ python/udemy/starter.py
@@ -7,13 +7,14 @@ random_number = random.randint(1,10)  # numbers 1 - 10
 #     otherwise tell them if they are too high or too low
 
 # BONUS - let player play again if they want!
-guess = input("Guess a number between 1 and 10: ")
-guess = int(guess)
-while random_number != guess:
-	if random_number > guess:
+guess = None
+while guess!=random_number:
+	guess = input("Guess a number between 1 and 10: ")
+	guess = int(guess)
+	if guess>random_number:
 		print("Too high, try again!")
-	else:
+	elif guess<random_number:
 		print("Too low, try again!")
-	guess = int(input("Guess a number between 1 and 10: "))
-else:
-	print("You guessed it, you won!")
+	else:
+		print("You won!")
+print(random_number)
