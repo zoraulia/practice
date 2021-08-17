a815763c816ac1ebf2e4a8c7ace1b9a372e6d13f
zoraulia <56012719+zoraulia@users.noreply.github.com>
2021-08-17 12:11:12 +0700

    finish exercise10

--- python/udemy/exercise10.py
+++ python/udemy/exercise10.py
@@ -2,4 +2,4 @@ list1 = ["CA", "NJ", "RI"]
 list2 = ["California", "New Jersey", "Rhode Island"]
 
 # make sure your solution is assigned to the answer variable so the tests can work!
-answer = {}
+answer = dict(zip(list1,list2))
