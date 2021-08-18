59f13651bdf4e7fd0d6c3df5c5b443a92593e174
zoraulia <56012719+zoraulia@users.noreply.github.com>
2021-08-18 19:07:34 +0700

    finish exercise19

--- python/udemy/exercise19.py
+++ python/udemy/exercise19.py
@@ -32,3 +32,78 @@ def return_day(num):
 
 
 # problem 3
+'''
+last_element([1,2,3]) # 3
+last_element([]) # None
+'''
+
+def last_element(elem):
+    if elem==[]:
+        return None
+    return elem[-1]
+
+
+# problem 4
+'''
+number_compare(1,1) # "Numbers are equal"
+number_compare(1,0) # "First is greater"
+number_compare(2,4) # "Second is greater"
+'''
+
+def number_compare(num1,num2):
+    if num1==num2:
+        return "Numbers are equal"
+    elif num1>num2:
+        return "First is greater"
+    return "Second is greater"
+
+
+# problem 5
+'''
+single_letter_count("Hello World", "h") # 1
+single_letter_count("Hello World", "z") # 0
+single_letter_count("HelLo World", "l") # 3
+'''
+
+#define single_letter_count below:
+def single_letter_count(words, num):
+    words = words.lower()
+    return words.count(num.lower())
+
+
+# problem 6
+'''
+multiple_letter_count("awesome") # {'a': 1, 'e': 2, 'm': 1, 'o': 1, 's': 1, 'w': 1}
+'''
+
+# flesh out multiple_letter count:
+def multiple_letter_count(letter):
+    return {k:letter.count(k) for k in letter}
+
+
+# problem 7
+'''
+list_manipulation([1,2,3], "remove", "end") # 3
+list_manipulation([1,2,3], "remove", "beginning") #  1
+list_manipulation([1,2,3], "add", "beginning", 20) #  [20,1,2,3]
+list_manipulation([1,2,3], "add", "end", 30) #  [1,2,3,30]
+'''
+
+def list_manipulation(list, command, loc, val=0):
+    if command=="remove" and loc=="end":
+        e = list.pop()
+        return e
+    elif command=="remove" and loc=="beginning":
+        b = list.pop(0)
+        return b
+    elif command=="add" and loc=="beginning":
+        list.insert(0, val)
+        return list
+    elif command=="add" and loc=="end":
+        list.append(val)
+        return list
+
+print(list_manipulation([1,2,3], "remove", "end")) # 3
+print(list_manipulation([1,2,3], "remove", "beginning")) #  1
+print(list_manipulation([1,2,3], "add", "beginning", 20)) #  [20,1,2,3]
+print(list_manipulation([1,2,3], "add", "end", 30)) #  [1,2,3,30]
