db2086aced88463976875f7afb638fa7a9779897
zoraulia <56012719+zoraulia@users.noreply.github.com>
2021-08-18 10:27:58 +0700

    modify exercise18 file

--- python/udemy/exercise18.py
+++ python/udemy/exercise18.py
@@ -1,3 +1,10 @@
-#Yell function
-def yell(word):
-    return "{}!".format(word.upper())
+# #Yell function
+# def yell(word):
+#     return "{}!".format(word.upper())
+
+# Define speak below:
+def speak(animal='dog'):
+    noises = {'pig':'oink', 'duck':'quack', 'cat':'meow', 'dog':'woof'}
+    return noises.get(animal, '?')
+
+print(speak("banana"))
